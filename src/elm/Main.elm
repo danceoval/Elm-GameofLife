209c2ae765a138f4564e5c67a8d4a85735eaa67e
Dan Sohval <dan@Dans-MacBook-Pro-2.local>
2017-01-24 17:00:17 -0500

    initial

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -2,62 +2,101 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing ( onClick )
+import Array exposing (..)
 
--- component import example
-import Components.Hello exposing ( hello )
 
 
 -- APP
-main : Program Never Int Msg
+main : Program Never Model Msg
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- MODEL
-type alias Model = Int
+type alias Point = {
+  index : Int,
+  x : Int,
+  y : Int,
+  status : Bool
+}
 
-model : number
-model = 0
+type alias Model = {
+ points : Array Point
+}
 
+model : Model
+model = {
+  points = Array.initialize 64 (\idx -> {
+      index = idx,
+      x = idx % 8,
+      y = (idx // 8) % 8,
+      status = False
+    })
+  }
+
+-- UTILS
+getStatus: Point -> String
+getStatus point =
+  if point.status == True then
+    "alive"
+  else
+    "dead"  
+
+placeholder : Point
+placeholder = {
+    x = 0,
+    y = 0,
+    index = 0,
+    status = False
+  }
 
 -- UPDATE
-type Msg = NoOp | Increment
+type Msg = ToggleCell Int 
+          | NoOp
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
+    ToggleCell idx ->
+      let 
+        point = 
+          Maybe.withDefault placeholder (Array.get idx model.points)
+        newPoints = 
+          Array.set idx {point | status = (not point.status) } model.points  
+      in
+        {model | points = newPoints}
     NoOp -> model
-    Increment -> model + 1
 
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
   div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
+    h1 [] [text "Game of Life"],
+    table [id "board"] [
+      tbody [] [
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 0 8 model.points))),
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 8 16 model.points))),
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 16 24 model.points))),
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 24 32 model.points))),
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 32 40 model.points))),
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 40 48 model.points))),
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 48 56 model.points))),
+        tr [] 
+          (List.map(\point -> td [class (getStatus point), onClick (ToggleCell point.index)] [] ) (Array.toList (Array.slice 56 64 model.points)))      
       ]
+    ],
+    div [id "control_panel"] [
+      button [id "step_btn", classList [("btn", True), ("btn-success", True)]] [text "Step"],
+      button [id "play_btn", classList [("btn", True), ("btn-primary", True)]] [text "Play"],
+      button [id "reset_btn", classList [("btn", True), ("btn-warning", True)]] [text "Reset"],
+      button [id "clear_btn", classList [("btn", True), ("btn-info", True)]] [text "Clear"]
     ]
   ]
 
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
